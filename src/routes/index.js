f239b2c539d24f1219c670cd4c8d608584c0acf3
Your Name <you@example.com>
2019-08-02 21:41:29 +0800

    登录功能完成

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 // 职责：初始化路由对象  提供 main.js 使用
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import login from '@/views/login'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [
+    //   跳转路由 $router.push('/login) 或者 $router.push({name: 'login'})
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    }
+  ]
 })
 
 export default router
