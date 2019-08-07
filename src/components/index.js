00ec999d597fdf3961581f387e8e0d49ecc30817
Your Name <you@example.com>
2019-08-07 20:33:04 +0800

    插件的封装

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+// 封装一个 vue 插件
+// 作用：注册一个 components 下的所有的组件为全局组件
+import MyBread from '@/components/my-bread'
+
+// vue 插件定义的规则  暴露一个对象 {}  在对象选项中 install 对应的一个函数  函数形参 Vue 对象
+export default {
+  install (Vue) {
+    // Vue .use （使用插件）  vue 就是你 use 前的 vue
+    // Vue 全局注册组件即可
+    Vue.component(MyBread.name, MyBread)
+  }
+
+}
