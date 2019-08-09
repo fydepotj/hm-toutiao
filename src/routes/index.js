f17eab6ffa29d53ecf0ad7e07bcd97e3388ebad0
Your Name <you@example.com>
2019-08-09 17:38:39 +0800

    素材管理完成

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,8 @@ import Home from '@/views/home'
 import Welcome from '@/views/welcome'
 import Article from '@/views/article'
 import NotFound from '@/views/404'
+import Image from '@/views/image'
+
 import store from '../store'
 
 Vue.use(VueRouter)
@@ -20,7 +22,8 @@ const router = new VueRouter({
       component: Home,
       children: [
         { path: '/', name: 'welcome', component: Welcome },
-        { path: '/article', name: 'article', component: Article }
+        { path: '/article', name: 'article', component: Article },
+        { path: '/image', name: 'image', component: Image }
       ]
     },
     // 路径 404
